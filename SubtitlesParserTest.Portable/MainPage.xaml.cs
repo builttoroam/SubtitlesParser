09858db4073737f9795372d25941bccabd609146
Jay Sim <jay@builttoroam.com>
2017-09-09 00:55:17 +1000

    render subtitles in listviews (ttml & vtt)

--- SubtitlesParserTest.Portable/MainPage.xaml.cs
+++ SubtitlesParserTest.Portable/MainPage.xaml.cs
@@ -1,93 +1,57 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using BuildIt;
 using SubtitlesParserNew.Classes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Reflection;
 
 namespace SubtitlesParserTest.Portable
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage : ContentPage
 	{
 	    public MainPage()
 	    {
 	        InitializeComponent();
-	    }
 
-	    private void OnTTMLProcessed(object sender, EventArgs e)
-	    {
-	        try
-	        {
-	            Task.Run(() =>
-	            {
-	                var ttmlFile = "2518_aud_SpReco.ttml";
-	                ProcessSubtitle(ttmlFile);
-                });
-	        }
-	        catch (Exception ex)
-	        {
-	            ex.LogException();   
-	        }
-	    }
-
-	    private void OnVTTProcessed(object sender, EventArgs e)
-	    {
-	        try
-	        {
-	            Task.Run(() =>
-	            {
-	                var ttmlFile = "2518_aud_SpReco.vtt";
-	                ProcessSubtitle(ttmlFile);
-                });
-	        }
-	        catch (Exception ex)
-	        {
-	            ex.LogException();
-	        }
-	    }
+            var resourceIds = GetType().GetTypeInfo().Assembly.GetManifestResourceNames();
+            foreach(var resourceId in resourceIds)
+            {
+                ProcessSubtitle(resourceId);
+            }
+        }
 
 
-	    private void ProcessSubtitle(string filePath)
-	    {
-            Debug.WriteLine("ProcessSubtitle fired!");
+        private void ProcessSubtitle(string resourceId)
+        {
+            Debug.WriteLine($"Resource Id: {resourceId}");
+
+            var fileNames = resourceId.Split('.');
+            var fileName = fileNames.Length > 1 ?  $"{fileNames[fileNames.Length - 2]}.{fileNames[fileNames.Length - 1]}" : string.Empty;
+            Debug.WriteLine($"FileName: {fileName}");
+            var resourceStream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(resourceId);
 	        var parser = new SubtitlesParserNew.Classes.Parsers.SubParser();
-	        var fileName = Path.GetFileName(filePath);
-	        Debug.WriteLine($"File Read! {fileName}");
-
-            using (var fileStream = File.OpenRead(filePath))
-	        {
-	            Debug.WriteLine($"File Open Read! {filePath}");
-                try
-                {
-	                var mostLikelyFormat = parser.GetMostLikelyFormat(fileName);
-	                if (mostLikelyFormat == SubtitlesFormat.TtmlFormat)
-	                {
-	                    var ttmlItems = parser.ParseStream(fileStream, Encoding.UTF8, mostLikelyFormat);
-	                    ttmlCaption.Text = string.Join(", ", ttmlItems?.FirstOrDefault()?.Lines);
-	                }
-                    else if (mostLikelyFormat == SubtitlesFormat.WebVttFormat)
-	                {
-	                    var vttItems = parser.ParseStream(fileStream, Encoding.UTF8, mostLikelyFormat);
-	                    vttCaption.Text = string.Join(", ", vttItems?.FirstOrDefault()?.Lines);
-
-                    }
-                    //   var items = parser.ParseStream(fileStream, Encoding.UTF8, mostLikelyFormat);
-                    //if (items.Any())
-                    //{
-                    //Console.WriteLine("Parsing of file {0}: SUCCESS ({1} items - {2}% corrupted)",
-                    //    fileName, items.Count, (items.Count(it => it.StartTime <= 0 || it.EndTime <= 0) * 100) / items.Count);
-                    //}
-                }
-	            catch (Exception ex)
+            
+            try
+            {
+	            var mostLikelyFormat = parser.GetMostLikelyFormat(fileName);
+	            if (mostLikelyFormat == SubtitlesFormat.TtmlFormat)
 	            {
-                    ex.LogException();
-	            }
+	                var ttmlItems = parser.ParseStream(resourceStream, Encoding.UTF8, mostLikelyFormat);
+                    ttmlList.ItemsSource = ttmlItems;
+
+                }
+                else if (mostLikelyFormat == SubtitlesFormat.WebVttFormat)
+	            {
+	                var vttItems = parser.ParseStream(resourceStream, Encoding.UTF8, mostLikelyFormat);
+                    vttList.ItemsSource = vttItems;
+                }
+            }
+	        catch (Exception ex)
+	        {
+                ex.LogException();
 	        }
         }
     }
