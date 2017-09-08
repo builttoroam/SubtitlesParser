3b4c614b7f46bba83fac0b2102880c471e1920eb
Jay Sim <jay@builttoroam.com>
2017-09-08 17:34:23 +1000

    create class library that targets .net library create uwp sample project (but no test cases yet)

---
+++ SubtitlesParserTest.Portable/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SubtitlesParserTest.Portable
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+	    public MainPage()
+	    {
+	        InitializeComponent();
+	    }
+
+	    private void OnTTMLProcessed(object sender, EventArgs e)
+	    {
+	        
+	    }
+
+	    private void OnVTTProcessed(object sender, EventArgs e)
+	    {
+
+	    }
+
+    }
+}
